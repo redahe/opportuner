813a5effdae10f1ad53cd747c318ea84157fd871
reda <reda@localhost>
2017-01-05 21:40:38 +0300

    demo

--- modules/finance_scrape.py
+++ modules/finance_scrape.py
@@ -3,7 +3,7 @@
 import urllib2
 import json
 
-symbols = ['GOOG', 'AAPL', 'NVDA']
+symbols = ['GOOG', 'AAPL', 'NVDA', 'TSLA', 'IBM', 'KO']
 request_url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' +\
           '%22%2C%22'.join(symbols) +\
           '%22)%0A%09%09&format=json&diagnostics=true&env=http%3A%2F%2Fdatatables.org%2Falltables.env&callback='
@@ -20,5 +20,3 @@ for q in quotes:
     result['DY'] = q.get('DividendYield', None)
     result['SYMBOL'] = q.get('symbol', None)
     print json.dumps(result)
-
-
--- process.sh
+++ process.sh
@@ -1,4 +1,5 @@
 top_ops=15
 echo 'RANK   ID        SCORE           DATA'
 echo '-------------------------------------'
-modules/finance_scrape.py | modules/finance_range.py  | sort -rnk1  | head -n "$top_ops" | awk '{printf "%-6s %-9s %-15s %-50s\n",  NR, $2, $1, $0}'
+modules/finance_scrape.py | modules/finance_range.py  | sort -rnk1  | head -n "$top_ops" |\
+       	awk '{printf "%-6s %-9s %-15s",  NR, $2, $1; $1=""; $2=""; print $0}'
