c325b3a6d946680c8192989078a30eb69657ac98
reda <reda@localhost>
2017-01-16 01:58:51 +0300

    Adding filtering

--- commodity.sh
+++ commodity.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-top_ops=180
+top_ops=200
 
 
-cat modules/commodity_dest | modules/commodity_scrape.py | modules/commodity_range.py  | sort -rnk1  | head -n "$top_ops" | ./show_in_browser.sh
+cat modules/commodity_dest | modules/commodity_scrape.py |\
+ grep -f modules/commodity_filter -v -i |  modules/commodity_range.py |\
+ sort -rnk1  | head -n "$top_ops" | ./show_in_browser.sh
 
  # awk '{printf "%-6s %-15s",  NR, $2, $1=""; $2=""; print $0}'
