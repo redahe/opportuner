2375574edea6ba78480727edd03e301fc1654de6
reda <reda@localhost>
2017-11-11 20:15:08 +0300

    gif_formatter

--- gif_formatter.py
+++ gif_formatter.py
@@ -2,12 +2,16 @@
 
 import sys
 import json
+import os
 import urllib
 import cStringIO
 from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
 
 rank = 0
-frames = []
+
+output_directory = 'tmp'
 
 for line in sys.stdin.readlines():
     rank += 1
@@ -15,13 +19,11 @@ for line in sys.stdin.readlines():
     data = json.loads(dat)
     imgURL = data.pop('img', None)
     price = data.pop('price', '')
-
+    name = unicode(name, "utf-8")
+    price = unicode(price, "utf-8")
     fl = cStringIO.StringIO(urllib.urlopen(imgURL).read())
     img = Image.open(fl).convert('RGBA')
-    print(img.size)
-    print(img)
-    frames.append(img)
-
-frames[0].save(fp='out.gif', format='gif', save_all=True, duration=800, append_images=frames[1:])
-# writeGif('out.gif', frames)
-print("Done")
+    draw = ImageDraw.Draw(img)
+    fnt = ImageFont.truetype("DejaVuSans.ttf", 12)
+    draw.text((10, 10), name + ' ' + price, (0, 0, 255), font=fnt)
+    img.save(fp=output_directory + os.sep + str(rank-1)+'.png', format='png')
